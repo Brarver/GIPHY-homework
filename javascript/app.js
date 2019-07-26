2dac0e162b1a92b1ce1a9ea1af7650626793252c
brarver <brarver@yahoo.com>
2019-07-26 08:10:18 -0600

    starting local storage

--- javascript/app.js
+++ javascript/app.js
@@ -1,24 +1,33 @@
-var comedians = ['Will Ferrell', 'Chris Farley', 'Jim Carrey', 'Damon Waynes', 'Norm MacDonald', 'Wanda Sykes']
+var comedians = ['Will Ferrell', 'Sarah Silverman', 'Chris Farley', 'Jim Carrey', 'Damon Waynes', 'Norm MacDonald', 'Wanda Sykes']
 var comedian
 var favorites = []
 
+
+function saveFavorites() {
+  localStorage.clear()
+  localStorage.setItem('favorites', JSON.stringify(favorites))
+}
+
+function getFavorites() {
+  localStorage.getItem
+}
+
 function renderButtons() {
 
-    $(".buttons").empty();
+    $(".buttons").empty()
 
     for (var i = 0; i < comedians.length; i++) {
 
-      var a = $("<button>");
-      a.addClass("comedian-btn");
-      a.attr("data-name", comedians[i]);
-      a.text(comedians[i]);
-      $(".buttons").append(a);
-
+      var a = $("<button>")
+      a.addClass("comedian-btn")
+      a.attr("data-name", comedians[i])
+      a.text(comedians[i])
+      $(".buttons").append(a)
     }
   }
 
   function delGif() {
-    $('.rmv-fav').click(function (e) {
+    $('.rmv-fav').click(function () {
         $(this).siblings().remove()
         $(this).remove()
     })
@@ -28,22 +37,19 @@ function renderButtons() {
   function display() {
 
     $('.gifs').empty()
+    $('.gif-fav').css('display', 'block')
 
-    var comedian = $(this).attr("data-name");
+    var comedian = $(this).attr("data-name")
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=LhxwYFwsHo6Oph8eIbyKlGIctBkkB6sn&q=" + comedian + "&limit=10&offset=0&lang=en"
-    // var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=LhxwYFwsHo6Oph8eIbyKlGIctBkkB6sn&q=" + comedian + "&limit=10&offset=0&rating=G&lang=en"
     
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-        console.log(response)
 
         for (var i = 0; i < response.data.length; i++) {
-            console.log(response.data[0].images)
-
-            var stillUrl = response.data[i].images.fixed_width_still.url
-            var gifUrl = response.data[i].images.fixed_width.url
+            
+            var stillUrl = response.data[i].images.original_still.url
 
             var gifBox = $('<div class="gif-box">')
             gifBox.attr('data-num', [i])
@@ -69,31 +75,33 @@ function renderButtons() {
         $('.img-box').on("click", function () {
             var dataNum = $(this).attr('data-num')
             var state = $(this).attr('data-state')
-            // $(this).prev().remove()
-            console.log(this)
 
             if (state === 'still') {
                 
                 $(this).attr('data-state', 'animate')
-                $(this).attr('src', response.data[dataNum].images.fixed_width.url)
+                $(this).attr('src', response.data[dataNum].images.original.url)
                 
                 
             } else {
                 
-                $(this).attr('src', response.data[dataNum].images.fixed_width_still.url)
-                $(this).attr('data-state', 'still')
-                
-                
-                
+                $(this).attr('src', response.data[dataNum].images.original_still.url)
+                $(this).attr('data-state', 'still')   
             }    
         })
 
-        $('.add-fav').on("click", function (e) {
+        $('.add-fav').on("click", function () {
             var dataNum = $(this).attr('data-num')
 
             $(this).text('-Remove')
             $(this).removeClass('add-fav')
             $(this).addClass('rmv-fav')
+
+            $('.read').text('')
+
+            favorites.push($(this).parent())
+            console.log($(this).parent())
+            saveFavorites()
+            console.log(favorites)
             
             $('.gif-fav').append($(this).parent())
             $($(this).siblings()).attr('data-still', response.data[dataNum].images.fixed_width_still.url)
@@ -102,9 +110,7 @@ function renderButtons() {
             $($(this).siblings()).addClass('new-img-box')
             toggleFavGif()
             delGif()
-        })
-
-        
+        })   
     });
 
   }
@@ -139,12 +145,17 @@ $("#add-comedian").on("click", function(event) {
         renderButtons();
     }
 
-
   });
 
   $(document).on("click", ".comedian-btn", display);
 
   renderButtons()
 
-  //create clickable button for each added
+
+  //lines 6-11 and 101
+  
+
+
+
+  
   
